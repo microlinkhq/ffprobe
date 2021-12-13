63a5474a7af1a039d3ff43339a609c62b09ff6b6
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-12-13 15:21:01 +0100

    build: first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# ffprobe
+
+<p align="center">
+  <br>
+  <img src="https://i.imgur.com/Mh13XWB.gif" alt="ffprobe">
+  <br>
+</p>
+
+![Last version](https://img.shields.io/github/tag/microlinkhq/ffprobe.svg?style=flat-square)
+[![Coverage Status](https://img.shields.io/coveralls/microlinkhq/ffprobe.svg?style=flat-square)](https://coveralls.io/github/microlinkhq/ffprobe)
+[![NPM Status](https://img.shields.io/npm/dm/ffprobe.svg?style=flat-square)](https://www.npmjs.org/package/ffprobe)
+
+**NOTE:** more badges availables in [shields.io](https://shields.io/)
+
+> Static binaries for ffprobe
+
+## Install
+
+```bash
+$ npm install ffprobe --save
+```
+
+## Usage
+
+```js
+const ffprobe = require('ffprobe')
+
+ffprobe('do something')
+// => return something
+```
+
+## API
+
+### ffprobe(input, [options])
+
+#### input
+
+*Required*<br>
+Type: `string`
+
+Lorem ipsum.
+
+#### options
+
+##### foo
+
+Type: `boolean`<br>
+Default: `false`
+
+Lorem ipsum.
+
+## License
+
+**ffprobe** © [microlink.io](https://microlink.io), released under the [MIT](https://github.com/microlinkhq/ffprobe/blob/master/LICENSE.md) License.<br>
+Authored and maintained by [microlink.io](https://microlink.io) with help from [contributors](https://github.com/microlinkhq/ffprobe/contributors).
+
+> [microlink.io](https://microlink.io) · GitHub [microlink.io](https://github.com/microlinkhq) · Twitter [@microlinkhq](https://twitter.com/microlinkhq)
---
+++ scripts/postinstall.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const { mkdir, writeFile } = require('fs').promises
+const { promisify } = require('util')
+const lzma = require('lzma-native')
+const tar = require('tar-stream')
+const stream = require('stream')
+const fflate = require('fflate')
+const got = require('got')
+const os = require('os')
+
+const platform = process.env.FFPROBE_PLATFORM || process.platform
+const arch = process.env.FFPROBE_ARCH || os.arch()
+
+const URL = {
+  // mac m1
+  'darwin+arm64': 'https://cdn.microlink.io/ffprobe-4-4-1.tar.xz',
+  // linux production
+  'linux+x64':
+    'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz'
+}
+
+const pipeline = promisify(stream.pipeline)
+
+const main = async () => {
+  const extract = tar.extract()
+  let ffProbeData
+
+  extract.on('entry', (header, stream, cb) => {
+    const chunk = []
+    stream.on('data', data => chunk.push(data))
+    stream.on('end', () => {
+      const file = {
+        data: Buffer.concat(chunk),
+        mode: header.mode,
+        mtime: header.mtime,
+        path: header.name,
+        type: header.type
+      }
+
+      if (file.path.endsWith('ffprobe')) {
+        ffProbeData = file.data
+        requestStream.destroy(new Error('ABORTED_BY_USER'))
+      }
+      cb()
+    })
+  })
+
+  const requestStream = got.stream(URL[`${platform}+${arch}`])
+
+  try {
+    await pipeline(requestStream, lzma.Decompressor(), extract)
+  } catch (err) {
+    if (err.message !== 'ABORTED_BY_USER') throw err
+  }
+
+  await mkdir('bin')
+  await writeFile('bin/ffprobe', ffProbeData, { mode: 493 })
+}
+
+main()
+  .catch(err => console.error(err) && process.exit(1))
+  .then(process.exit)
