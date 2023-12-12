a722804ade0dab694435dcbb64d8e260663a23f0
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-12-12 23:25:21 +0100

    refactor: use tinyspawn (#29)

--- test/index.js
+++ test/index.js
@@ -1,35 +1,22 @@
 'use strict'
 
-const execa = require('execa')
+const $ = require('tinyspawn')
 const test = require('ava')
 
 const { path: ffprobePath } = require('..')
 
 test('expose ffprobe path', async t => {
-  const result = await execa(ffprobePath, ['-version'])
+  const result = await $(`${ffprobePath} --version`)
   console.log(result)
   t.is(result.exitCode, 0)
 })
 
-test('run arbitrary command', async t => {
-  const result = await execa(ffprobePath, [
-    '-print_format',
-    'json',
-    '-v',
-    'quiet',
-    '-show_format',
-    '-show_streams',
-    '-show_error',
-    'https://microlink.io/favicon.ico'
-  ])
-
-  console.log(result)
+test.only('run arbitrary command', async t => {
+  const cmd = `${ffprobePath} -print_format json -v quiet -show_format -show_streams -show_error https://microlink.io/favicon.ico`
+  const result = await $(cmd)
 
   t.is(result.exitCode, 0)
   t.true(!!result.stdout)
   t.false(!!result.stderr)
-  t.false(result.failed)
-  t.false(result.timedOut)
-  t.false(result.isCanceled)
   t.false(result.killed)
 })
